13edc3542198e10ea83f6914573f7103215decaa
MA7MOS <mahmod1w212@gmail.com>
2025-02-24 10:55:46 +0200

    lllll

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,27 +2,18 @@ package com.example.myapplication;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.Switch;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener , CompoundButton.OnCheckedChangeListener {
-ImageView image1 ;
-ImageView image2;;
-private  SeekBar sk;
-private int seekBarProgress;
-private Switch sw;
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener, CompoundButton.OnCheckedChangeListener {
+    ImageView image1;
+    private SeekBar sk;
+    private Switch sw;
 
-@Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -31,33 +22,23 @@ private Switch sw;
 
     @SuppressLint("CutPasteId")
     private void initViews() {
-        image1=findViewById(R.id.imageView);
-        image2=findViewById(R.id.imageView);
-        sw=findViewById(R.id.switch1);
-        sk=findViewById(R.id.seekBar);
+        image1 = findViewById(R.id.imageView);
+        sw = findViewById(R.id.switch1);
+        sk = findViewById(R.id.seekBar);
+
         sw.setOnCheckedChangeListener(this);
-
-
-    }
-
-
-
-    @Override
-    public void onClick(View v) {
-
+        sk.setOnSeekBarChangeListener(this);
     }
 
     @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
-
-
-
+        float translationX = progress * 2;
+        image1.setTranslationX(translationX);
     }
 
     @Override
     public void onStartTrackingTouch(SeekBar seekBar) {
-
     }
 
     @Override
@@ -65,14 +46,11 @@ private Switch sw;
 
     }
 
-
-
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        if(isChecked){
+        if (isChecked) {
             image1.setImageResource(R.drawable.sad);
-        }
-        else {
+        } else {
             image1.setImageResource(R.drawable.happy);
         }
     }
